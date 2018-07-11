9cf3a05839660bac17e0d0eb2a565e8d71c1f783
Seth Borne <sethborne@gmail.com>
2018-07-11 12:28:56 -0700

    revises previous github js file - for reset api key values and also turning these into static  class functions

--- customValidation_githubUser/js/github.js
+++ customValidation_githubUser/js/github.js
@@ -1,19 +1,21 @@
 // import { apiInfo } from '../apiKey/apiKey.js'
 
+const apiInfo_GitHubClientId = apiKey.getGitHubAPIClientID();
+// console.log(apiInfo_ClientId);
+const apiInfo_GitHubClientSecret = apiKey.getGitHubAPIClientSecret();
+// console.log(apiInfo_ClientSecret);
+
 class Github {
     constructor(){
         // this.client_id = apiInfo.clientID;
         // this.client_secret = apiInfo.clientSecret;
-        
-        this.client_id = '72c9c9b595f9140eb8b7';
-        this.client_secret = 'c7ce887abb4c59b4ba2dd6c6ce79410b4eca9190';
+        this.client_id = apiInfo_GitHubClientId;
+        this.client_secret = apiInfo_GitHubClientSecret;
     }
     // getUser Method
-    async getUser(user){
-        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`)
-        
+    async getUser(username){
+        const profileResponse = await fetch(`https://api.github.com/users/${username}?client_id=${this.client_id}&client_secret=${this.client_secret}`)
         const profileData = await profileResponse.json();
-        
         return {
             profile: profileData
         }
