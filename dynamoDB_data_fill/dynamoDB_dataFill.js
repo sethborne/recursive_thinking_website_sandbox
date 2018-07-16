6166a23f849157c3b63e98b0fe0972fc213d441e
Seth Borne <sethborne@gmail.com>
2018-07-15 18:07:39 -0700

    RTW Sandbox - DynamoDB Data Fill - First Commit

---
+++ dynamoDB_data_fill/dynamoDB_dataFill.js
@@ -0,0 +1,71 @@
+// let AWS = require('aws-sdk');
+// let docClient = new AWS.DynamoDB.DocumentClient({region: 'us-west-2'});
+let fs = require('fs')
+
+const allSkillsLanguageArray = [
+    ['javascript', 'JavaScript'],
+    ['html', 'HTML'],
+    ['html5', 'HTML5'],
+    ['css', 'CSS'],
+    ['css3', 'CSS3'],
+    ['jquery', 'jQuery'],
+    ['angularjs', 'AngularJS'],
+    ['angular', 'Angular'],
+    ['react', 'React'],
+    ['reactjs', 'ReactJS'],
+    ['csharp', 'C#'],
+    ['dotnetcore', '.NetCore'],
+    ['nancy', 'Nancy'],
+    ['python', 'Python'],
+    ['django', 'Django'],
+    ['flask', 'Flask'],
+    ['bootstrap', 'Bootstrap'],
+    ['webpack', 'WebPack'],
+]
+
+const allSkillsProfessionalArray = [
+    ['userexperiencedesign', 'User Experience Design'],
+    ['userresearch', 'User Research'],
+    ['informationarchitecture', 'Information Architecture'],
+    ['visualdesign', 'Visual Design'],
+    ['usercentereddesign', 'User Centered Design'],
+    ['wireframingdesign', 'Wireframing Design'],
+    ['interactiondesign', 'Interaction Design'],
+    ['frontenddevelopment', 'Front End Development']
+]
+
+const allSkillsSoftwareArray = [
+    ['sketch', 'Sketch'],
+    ['balsamiq', 'Balsamiq'],
+    ['omniGraffle', 'OmniGraffle'],
+    ['axure', 'Axure']
+]
+
+function buildJSONStringForSkillOutput(skillArray, skillTable){
+    let JSONString = {
+
+    }
+    JSONString[skillTable] = []
+    for(let i = 0; i < skillArray.length; i += 1){
+        let tempObj = {
+            PutRequest:{
+                Item: {
+                    Id: { S: skillArray[i][0]},
+                    value: {
+                        name: { S: skillArray[i][1]},
+                        users: { L: []}
+                    }
+                }
+            }
+        }
+        JSONString[skillTable].push(tempObj)
+    }
+    // console.log(JSONString);
+    let JSONString1 = JSON.stringify(JSONString)
+    console.log(JSONString1);
+    fs.writeFileSync(`${skillTable}.json`, JSONString1, 'utf8')
+}
+
+buildJSONStringForSkillOutput(allSkillsLanguageArray, 'RecursiveThinkingProfileSkillsLanguage')
+buildJSONStringForSkillOutput(allSkillsProfessionalArray, 'RecursiveThinkingProfileSkillsProfessional')
+buildJSONStringForSkillOutput(allSkillsSoftwareArray, 'RecursiveThinkingProfileSkillsSoftware')
