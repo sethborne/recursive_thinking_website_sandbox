36314e6aab9cae4b9226685625038dd8ed9195ee
Seth Borne <sethborne@gmail.com>
2018-07-23 21:31:30 -0700

    updates script - homeScreen Quotes

--- dynamoDB_data_fill_homeScreen_quotes/dynamoDB_dataFill_homeScreen_quotes.js
+++ dynamoDB_data_fill_homeScreen_quotes/dynamoDB_dataFill_homeScreen_quotes.js
@@ -1,76 +1,79 @@
 let fs = require('fs');
 const uuidv1 = require('uuid/v1');
 
+let readJSONFromUserIdFile = fs.readFileSync('../dynamoDB_mock_data_returns/RecursiveThinkingDeveloperProfilesIdArray.json', 'utf8');
+let currentIdsForUsers = JSON.parse(readJSONFromUserIdFile)
+
 const allHomeScreenQuotesArray = [
     [
         [ 'Id' ],
         [ 'quote', 'Bacon ipsum dolor amet porchetta bacon shank, tri-tip pancetta ground round sausage t-bone. Venison bacon short ribs pastrami. Porchetta jerky doner frankfurter beef ribs. Spare ribs tail cupim t-bone kevin ribeye alcatra shankle bacon meatball bresaola.'],
         [ 'createdAt', new Date('2018-06-14T12:00:00Z').toString() ],
         [ 'updatedAt', new Date('2018-07-14T12:00:00Z').toString() ],
-        [ '_createdByUser' ]
+        [ '_createdByUser', currentIdsForUsers[0] ]
     ],
     [
         [ 'Id' ],
         [ 'quote', 'Stick butt in face. My left donut is missing, as is my right run outside as soon as door open for eat and than sleep on your face and then cats take over the world. Drool i could pee on this if i had the energy instead of drinking water from the cat bowl, make sure to steal water from the toilet yet cough hairball on conveniently placed pants so attack feet. What a cat-ass-trophy!'],
         [ 'createdAt', new Date('2018-06-16T12:00:00Z').toString() ],
         [ 'updatedAt', new Date('2018-07-16T12:00:00Z').toString() ],
-        [ '_createdByUser' ]
-    ],
-    [
-        [ 'Id' ],
-        [ 'quote', 'Sweet sweet chupa chups jelly ice cream oat cake croissant cake. Jelly-o marzipan tart oat cake chocolate muffin sesame snaps dragée sweet. Pudding oat cake marzipan icing toffee dessert. Carrot cake bonbon muffin tootsie roll. Candy ice cream pudding caramels gummies pie tootsie roll. Macaroon marshmallow gummi bears donut toffee cake.'],
-        [ 'createdAt', new Date('2018-06-18T12:00:00Z').toString() ],
-        [ 'updatedAt', new Date('2018-07-18T12:00:00Z').toString() ],
-        [ '_createdByUser' ]
-    ],
-    [
-        [ 'Id' ],
-        [ 'quote', 'Why? I don\'t want to talk to you no more, you empty-headed animal food trough water! I fart in your general direction! Your mother was a hamster and your father smelt of elderberries! Now leave before I am forced to taunt you a second time! Are you suggesting that coconuts migrate?'],
-        [ 'createdAt', new Date('2018-06-20T12:00:00Z').toString() ],
-        [ 'updatedAt', new Date('2018-07-20T12:00:00Z').toString() ],,
-        [ '_createdByUser' ]
+        [ '_createdByUser', currentIdsForUsers[1] ]
     ],
     [
         [ 'Id' ],
         [ 'quote', 'You could sit here for weeks with your one hair brush trying to do that - or you could do it with one stroke with an almighty brush. Now let\'s put some happy little clouds in here. You\'re meant to have fun in life. Maybe there was an old trapper that lived out here and maybe one day he went to check his beaver traps, and maybe he fell into the river and drowned.'],
+        [ 'createdAt', new Date('2018-06-18T12:00:00Z').toString() ],
+        [ 'updatedAt', new Date('2018-07-18T12:00:00Z').toString() ],
+        [ '_createdByUser', currentIdsForUsers[4] ]
+    ],
+    [
+        [ 'Id' ],
+        [ 'quote', 'Bring a spring upon her cable holystone blow the man down spanker Shiver me timbers to go on account lookout wherry doubloon chase. Belay yo-ho-ho keelhaul squiffy black spot yardarm spyglass sheet transom heave to. Trysail Sail ho Corsair red ensign hulk smartly boom jib rum gangway. Case shot Shiver me timbers gangplank crack Jennys tea cup ballast Blimey lee snow crow\'s nest rutters. Fluke jib scourge of the seven seas boatswain schooner gaff booty Jack Tar transom spirits.'],
+        [ 'createdAt', new Date('2018-06-20T12:00:00Z').toString() ],
+        [ 'updatedAt', new Date('2018-07-20T12:00:00Z').toString() ],
+        [ '_createdByUser', currentIdsForUsers[7] ]
+    ],
+    [
+        [ 'Id' ],
+        [ 'quote', 'Now that we know who you are, I know who I am. I\'m not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain\'s going to be? He\'s the exact opposite of the hero. And most times they\'re friends, like you and me! I should\'ve known way back when... You know why, David? Because of the kids. They called me Mr Glass.'],
         [ 'createdAt', new Date('2018-06-22T12:00:00Z').toString() ],
         [ 'updatedAt', new Date('2018-07-22T12:00:00Z').toString() ],
-        [ '_createdByUser' ]
+        [ '_createdByUser', currentIdsForUsers[9] ]
     ],
     [
         [ 'Id' ],
-        [ 'quote', 'Unicorn sustainable taxidermy bitters man bun. Meh prism austin typewriter. Whatever iPhone aesthetic meh, four dollar toast post-ironic quinoa. Flexitarian migas tumeric pinterest craft beer. VHS irony cloud bread viral, swag bitters selfies scenester shoreditch disrupt chambray mustache. Food truck keffiyeh seitan hashtag man braid tumblr.'],
+        [ 'quote', 'I think you\'ve let your personal feelings cloud your judgement. Why don\'t we just give everybody a promotion and call it a night - \'Commander\'? Yesterday I did not know how to eat gagh. Sure. You\'d be surprised how far a hug goes with Geordi, or Worf. Your shields were failing, sir. About four years. I got tired of hearing how young I looked. Besides, you look good in a dress.'],
         [ 'createdAt', new Date('2018-06-24T12:00:00Z').toString() ],
         [ 'updatedAt', new Date('2018-07-24T12:00:00Z').toString() ],
-        [ '_createdByUser' ]
+        [ '_createdByUser', currentIdsForUsers[11] ]
     ],
     [
         [ 'Id' ],
-        [ 'quote', 'Parallel path work so where do we stand on the latest client ask get six alpha pups in here for a focus group, nor high touch client social currency. We need to leverage our synergies high-level so lean into that problem yet are there any leftovers in the kitchen? nor bake it in dog and pony show cannibalize.'],
+        [ 'quote', 'We\'re both adults. I can\'t pretend I don\'t know that person is you. I want there to be no confusion. I know I owe you my life. And more than that, I respect the strategy. In your position, I would have done the same. One issue, which troubles me, I don\'t know what happens when our three-month contract ends. You know why I do this. I want security for my family.'],
         [ 'createdAt', new Date('2018-06-26T12:00:00Z').toString() ],
         [ 'updatedAt', new Date('2018-07-26T12:00:00Z').toString() ],
-        [ '_createdByUser' ]
+        [ '_createdByUser', currentIdsForUsers[13] ]
     ],
     [
         [ 'Id' ],
-        [ 'quote', 'This looks perfect. Just Photoshop out the dog, add a baby, and make the curtains blue needs to be sleeker I really like the colour but can you change it can you make it look more designed . I think we need to start from scratch I like it, but can the snow look a little warmer.'],
+        [ 'quote', 'Hodor, hodor... Hodor hodor hodor. Hodor hodor hodor? Hodor! Hodor; hodor hodor hodor hodor? Hodor hodor hodor? Hodor! Hodor hodor. HODOR! Hodor, hodor hodor, hodor? Hodor! Hodor hodor, hodor hodor hodor? Hodor! HODOR!'],
         [ 'createdAt', new Date('2018-06-28T12:00:00Z').toString() ],
         [ 'updatedAt', new Date('2018-07-28T12:00:00Z').toString() ],
-        [ '_createdByUser' ]
+        [ '_createdByUser', currentIdsForUsers[15] ]
     ],
     [
         [ 'Id' ],
-        [ 'quote', 'Pink schooner hands scallywag Jack Tar broadside salmagundi lass Sail ho hogshead. Interloper walk the plank bilged on her anchor plunder scallywag bowsprit sutler stern lugger rigging. Carouser barque landlubber or just lubber pink swab log trysail aye Jolly Roger snow.'],
+        [ 'quote', 'We found out you used to be a dog-faced boy. I scream, you scream, we all scream for nonfat Tofutti rice dreamsicles. You ever seen a UFO in these parts? Scully, you\'re not going to believe this. Something Weird. A UFO Party. I have a life. I saw Elvis in a potato chip once. Whatever tape you found in that VCR, it isn\'t mine.'],
         [ 'createdAt', new Date('2018-06-30T12:00:00Z').toString() ],
         [ 'updatedAt', new Date('2018-07-30T12:00:00Z').toString() ],
-        [ '_createdByUser' ]
+        [ '_createdByUser', currentIdsForUsers[16] ]
     ],
     [
         [ 'Id' ],
-        [ 'quote', 'Pommy ipsum flog River Song stupendous, jolly good how\'s your father. Spend a penny brown sauce Essex girls sorted it picalilly jellied eels stop arsing around, know your onions a bit miffed bloody mary stew and dumps on his tod. Biscuits wellies chippy collywobbles warts and all, get away with ya conked him one on the nose what a load of cobblers.'],
+        [ 'quote', 'When Chuck Norris does a pushup, he isn\'t lifting himself up, he\'s pushing the Earth down, Contrary to popular belief, America is not a democracy, it is a Chucktatorship The chief export of Chuck Norris is Pain Chuck Norris doesn\'t read books. He stares them down until he gets the information he wants.'],
         [ 'createdAt', new Date('2018-07-02T12:00:00Z').toString() ],
         [ 'updatedAt', new Date('2018-08-02T12:00:00Z').toString() ],
-        [ '_createdByUser' ]
+        [ '_createdByUser', currentIdsForUsers[19] ]
     ]
 ]
 
@@ -96,7 +99,7 @@ function buildJSONStringForHomeScreenQuotesOutput(homeScreenQuotesArray, homeScr
         // updatedAt
         tempObj['PutRequest']['Item'][homeScreenQuotesArray[i][3][0]] = { "S": homeScreenQuotesArray[i][3][1]};
         // _createdByUser
-        // tempObj['PutRequest']['Item'][homeScreenQuotesArray[i][4][0]] = { "S": homeScreenQuotesArray[i][4][1]};               
+        tempObj['PutRequest']['Item'][homeScreenQuotesArray[i][4][0]] = { "S": homeScreenQuotesArray[i][4][1]};               
         string[homeScreenQuotesTable].push(tempObj)
     }
     // console.log(string);
