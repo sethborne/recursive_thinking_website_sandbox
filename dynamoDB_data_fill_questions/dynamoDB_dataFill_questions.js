8b185031c906a9d2aa6a32cdaf7ec8a1060827d4
Seth Borne <sethborne@gmail.com>
2018-07-23 21:32:17 -0700

    updates script and data object - interviewQuestion

--- dynamoDB_data_fill_questions/dynamoDB_dataFill_questions.js
+++ dynamoDB_data_fill_questions/dynamoDB_dataFill_questions.js
@@ -3,7 +3,7 @@ let AWS = require('aws-sdk');
 let util = require('util');
 const uuidv1 = require('uuid/v1');
 
-let dateFunction = require('../all_functions/all_functions.js');
+let allFunctions = require('../all_functions/all_functions.js');
 // console.log('in question', dateFunction.shiftDays('before', 45).toString());
 
 let readJSONFromUserIdFile = fs.readFileSync('../dynamoDB_mock_data_returns/RecursiveThinkingDeveloperProfilesIdArray.json', 'utf8');
@@ -14,6 +14,10 @@ let readJSONFromInterviewQuestionsIdFile = fs.readFileSync('../dynamoDB_mock_dat
 let currentIdsForInterviewQuestions = JSON.parse(readJSONFromInterviewQuestionsIdFile)
 console.log(currentIdsForInterviewQuestions);
 
+let readJSONFromInterviewQuestionsAnswersIdFile = fs.readFileSync('../dynamoDB_mock_data_returns/RecursiveThinkingInterviewQuestionsAnswersIdArray.json', 'utf8');
+let currentIdsForInterviewQuestionsAnswers = JSON.parse(readJSONFromInterviewQuestionsAnswersIdFile)
+console.log(currentIdsForInterviewQuestionsAnswers.length);
+
 const allQuestionsArray = [
     [
         [ 'Id' ],
@@ -22,8 +26,8 @@ const allQuestionsArray = [
         ['description', 'Diversify kpis. We need to socialize the comms with the wider stakeholder community even dead cats bounce. Core competencies not the long pole in my tent. We have got to manage that low hanging fruit show pony quick win I just wanted to give you a heads-up can we align on lunch orders. We need to touch base off-line before we fire the new ux experience drop-dead date, but strategic fit, yet thought shower lean into that problem high turnaround rate. Future-proof high turnaround rate today shall be a cloudy day, thanks to blue sky thinking, we can now deploy our new ui to the cloud , nor can I just chime in on that one.'],
         [ 'categories', [] ],
         [ 'answersToQuestion', []],
-        [ 'createdAt', new Date('2018-06-14T12:00:00Z').toString() ],
-        [ 'updatedAt', new Date('2018-07-14T12:00:00Z').toString() ],
+        [ 'createdAt', new Date('2018-08-14T12:00:00Z').toString() ],
+        [ 'updatedAt', new Date('2018-08-14T12:00:00Z').toString() ],
         [ '_createdByUser' ]
     ],
     [
@@ -33,8 +37,8 @@ const allQuestionsArray = [
         ['description', 'Quarter killick gally reef sutler quarterdeck tack aye lookout Pieces of Eight. Heave down Yellow Jack scuttle league stern jack boatswain cog lee ahoy. Capstan Sea Legs lass booty strike colors spirits hardtack pink reef crack Jennys tea cup. Quarter deadlights pink yo-ho-ho fore run a rig overhaul lateen sail hogshead lookout. Lee fluke fathom Pieces of Eight sheet Sail ho ye six pounders pink pirate. Chase broadside cable reef sails gabion salmagundi cutlass fathom ho sheet. Scurvy Nelsons folly sheet tender cable American Main red ensign hogshead dance the hempen jig ahoy. Boom pressgang Sail ho reef sails coxswain piracy bilge water port sheet killick. Knave Jack Ketch deadlights clap of thunder scurvy crimp marooned fluke Buccaneer lee.'],
         [ 'categories', [] ],
         [ 'answersToQuestion', []],
-        [ 'createdAt', new Date('2018-06-16T12:00:00Z').toString() ],
-        [ 'updatedAt', new Date('2018-07-16T12:00:00Z').toString() ],
+        [ 'createdAt', new Date('2018-08-16T12:00:00Z').toString() ],
+        [ 'updatedAt', new Date('2018-08-16T12:00:00Z').toString() ],
         [ '_createdByUser' ]
     ],
     [
@@ -44,8 +48,8 @@ const allQuestionsArray = [
         ['description', 'Stirred by starlight. Corpus callosum great turbulent clouds, from which we spring brain is the seed of intelligence concept of the number one vastness is bearable only through love. Decipherment. Emerged into consciousness? Rings of Uranus courage of our questions white dwarf tesseract, Drake Equation Flatland, cosmos inconspicuous motes of rock and gas hearts of the stars the ash of stellar alchemy, emerged into consciousness. Made in the interiors of collapsing stars Sea of Tranquility realm of the galaxies. Of brilliant syntheses. Tingling of the spine with pretty stories for which there is little good evidence. Cosmic fugue at the edge of forever a billion trillion, across the centuries galaxies with pretty stories for which there is little good evidence, trillion, galaxies, the carbon in our apple pies quasar, permanence of the stars at the edge of forever billions upon billions rogue.'],
         [ 'categories', [] ],
         [ 'answersToQuestion', []],
-        [ 'createdAt', new Date('2018-06-18T12:00:00Z').toString() ],
-        [ 'updatedAt', new Date('2018-07-18T12:00:00Z').toString() ],
+        [ 'createdAt', new Date('2018-08-18T12:00:00Z').toString() ],
+        [ 'updatedAt', new Date('2018-08-18T12:00:00Z').toString() ],
         [ '_createdByUser' ]
     ],
     [
@@ -55,8 +59,8 @@ const allQuestionsArray = [
         ['description', 'You think water moves fast? You should see ice. It moves like it has a mind. Like it knows it killed the world once and got a taste for murder. After the avalanche, it took us a week to climb out. Now, I do not know exactly when we turned on each other, but I know that seven of us survived the slide... and only five made it out. Now we took an oath, that I am breaking now. We said we would say it was the snow that killed the other two, but it was not. Nature is lethal but it does not hold a candle to man. Look, just because I do not be giving no man a foot massage do not make it right for Marsellus to throw Antwone into a glass motherfucking house, fucking up the way the nigger talks. Motherfucker do that shit to me, he better paralyze my ass, because I will kill the motherfucker, know what I am saying'],
         [ 'categories', [] ],
         [ 'answersToQuestion', []],
-        [ 'createdAt', new Date('2018-06-20T12:00:00Z').toString() ],
-        [ 'updatedAt', new Date('2018-07-20T12:00:00Z').toString() ],
+        [ 'createdAt', new Date('2018-08-20T12:00:00Z').toString() ],
+        [ 'updatedAt', new Date('2018-08-20T12:00:00Z').toString() ],
         [ '_createdByUser' ]
     ]
 ]
@@ -88,15 +92,12 @@ else{
     fs.writeFileSync(`../dynamoDB_mock_data_returns/RecursiveThinkingInterviewQuestionsIdArray.json`, questionsIdArray, 'utf8')
 }
 
-
-
 function buildJSONStringForLessonOutput(questionArray, questionTable){
     let string = {
 
     }
     string[questionTable] = []
     for(let i = 0; i < questionArray.length; i += 1){
-        
         let tempObj = {
             PutRequest:{
                 Item: {
@@ -104,8 +105,7 @@ function buildJSONStringForLessonOutput(questionArray, questionTable){
             }
         }
         // ID      
-        tempObj['PutRequest']['Item'][questionArray[i][0][0]] = { "S": uuidv1() };
-        // console.log('GenId', uuidv1());
+        tempObj['PutRequest']['Item'][questionArray[i][0][0]] = { "S": currentIdsForInterviewQuestions[i] };
         // Title
         tempObj['PutRequest']['Item'][questionArray[i][1][0]] = { "S": questionArray[i][1][1]};
         // Submitted
@@ -113,16 +113,31 @@ function buildJSONStringForLessonOutput(questionArray, questionTable){
         // Description
         tempObj['PutRequest']['Item'][questionArray[i][3][0]] = { "S": questionArray[i][3][1]};
         // Categories
-        tempObj['PutRequest']['Item'][questionArray[i][4][0]] = { "L": questionArray[i][4][1]};        
+        tempObj['PutRequest']['Item'][questionArray[i][4][0]] = { "L": questionArray[i][4][1]};
         // AnswersToQuestion
-        tempObj['PutRequest']['Item'][questionArray[i][5][0]] = { "L": questionArray[i][5][1]};
+        // need to return an array
+        let answersToQuestionArray = allFunctions.getArrayOfValuesAtAFixedLength(currentIdsForInterviewQuestionsAnswers, 2);
+        let answersToPushArray = []
+        for(let a = 0; a < answersToQuestionArray.length; a += 1){
+            // console.log(t);
+            let tempString = {
+                "S": answersToQuestionArray[a]
+            }
+            // console.log(tempString);
+            answersToPushArray.push(tempString)
+        }
+        console.log(answersToPushArray);
+        tempObj['PutRequest']['Item'][questionArray[i][5][0]] = { "L": answersToPushArray};
         // createdAt
         tempObj['PutRequest']['Item'][questionArray[i][6][0]] = { "S": questionArray[i][6][1]};
         // updatedAt
         tempObj['PutRequest']['Item'][questionArray[i][7][0]] = { "S": questionArray[i][7][1]};        
         // _createdByUser
-        // we need a random 
-        // tempObj['PutRequest']['Item'][questionArray[i][8][0]] = { "S": ""};              
+        // need a random index, then push that index to the value
+        let randomIndexCreatedBy = allFunctions.getRandomIndexOfArray(currentIdsForUsers.length);
+        // console.log(currentIdsForUsers);
+        // console.log("hi", randomIndexCreatedBy);
+        tempObj['PutRequest']['Item'][questionArray[i][8][0]] = { "S": currentIdsForUsers[randomIndexCreatedBy]};           
         string[questionTable].push(tempObj)
     }
     // console.log(string);
