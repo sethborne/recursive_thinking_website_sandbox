6732924ea4695fe4e091da74e18dac89140972b2
Seth Borne <sethborne@gmail.com>
2018-09-06 15:21:21 -0700

    updates DynamoDB autofill script - Skills

--- dynamoDB_data_fill/dynamoDB_dataFill.js
+++ dynamoDB_data_fill/dynamoDB_dataFill.js
@@ -1,63 +1,94 @@
 // let AWS = require('aws-sdk');
 // let docClient = new AWS.DynamoDB.DocumentClient({region: 'us-west-2'});
-let fs = require('fs')
+let fs = require('fs');
+let AWS = require('aws-sdk');
+let util = require('util');
+const uuidv1 = require('uuid/v1');
 
-const allSkillsLanguageArray = [
-    ['javascript', 'JavaScript'],
-    ['html', 'HTML'],
-    ['html5', 'HTML5'],
-    ['css', 'CSS'],
-    ['css3', 'CSS3'],
-    ['jquery', 'jQuery'],
-    ['angularjs', 'AngularJS'],
-    ['angular', 'Angular'],
-    ['react', 'React'],
-    ['reactjs', 'ReactJS'],
-    ['csharp', 'C#'],
-    ['dotnetcore', '.NetCore'],
-    ['nancy', 'Nancy'],
-    ['python', 'Python'],
-    ['django', 'Django'],
-    ['flask', 'Flask'],
-    ['bootstrap', 'Bootstrap'],
-    ['webpack', 'WebPack'],
+let allFunctions = require('../all_functions/all_functions.js');
+
+let readJSONFromUserIdFile = fs.readFileSync('../dynamoDB_mock_data_returns/RecursiveThinkingDeveloperProfilesIdArray.json', 'utf8');
+let currentIdsForUsers = JSON.parse(readJSONFromUserIdFile)
+console.log('AllUsers', currentIdsForUsers);
+
+const allSkillsLanguageArray = [ 'JavaScript', 'HTML', 'HTML5', 'CSS', 'CSS3', 'jQuery', 'AngularJS', 'Angular', 'React', 'ReactJS', 'C#', '.NetCore', 'Nancy', 'Python', 'Django', 'Flask', 'Bootstrap', 'WebPack' ]
+
+const allSkillsProfessionalArray = [ 'User Experience Design', 'User Research', 'Information Architecture', 'Visual Design', 'User Centered Design', 'Wireframing Design', 'Interaction Design', 'Front End Development']
+
+const allSkillsSoftwareArray = [ 'Sketch', 'Balsamiq', 'OmniGraffle', 'Axure']
+
+// =========================================================================
+// Create the file
+// =========================================================================
+const textArray = [ 
+  [allSkillsLanguageArray, 'RecursiveThinkingProfileSkillsLanguagesIdArray.json'],
+  [allSkillsProfessionalArray, 'RecursiveThinkingProfileSkillsProfessionalIdArray.json'],
+  [allSkillsSoftwareArray, 'RecursiveThinkingProfileSkillsSoftwareIdArray.json']
 ]
+// console.log(allQuestionsArray.length);
+// textArray.forEach(arrItem => {
+//   let tempArr = [];
+//   arrItem[0].forEach(skillItem => tempArr.push(uuidv1()));
+//   tempArr = JSON.stringify(tempArr)
+//   fs.writeFileSync(`../dynamoDB_mock_data_returns/${arrItem[1]}`, tempArr, 'utf8')
+// })
 
-const allSkillsProfessionalArray = [
-    ['userexperiencedesign', 'User Experience Design'],
-    ['userresearch', 'User Research'],
-    ['informationarchitecture', 'Information Architecture'],
-    ['visualdesign', 'Visual Design'],
-    ['usercentereddesign', 'User Centered Design'],
-    ['wireframingdesign', 'Wireframing Design'],
-    ['interactiondesign', 'Interaction Design'],
-    ['frontenddevelopment', 'Front End Development']
-]
+// =========================================================================
 
-const allSkillsSoftwareArray = [
-    ['sketch', 'Sketch'],
-    ['balsamiq', 'Balsamiq'],
-    ['omniGraffle', 'OmniGraffle'],
-    ['axure', 'Axure']
-]
+// ref in each skill Id 
+let currentIdsForSkillsLanguages = [];
+let currentIdsForSkillsProfessional = [];
+let currentIdsForSkillsSoftware = [];
+let skillsIdArray = [ currentIdsForSkillsLanguages, currentIdsForSkillsProfessional, currentIdsForSkillsSoftware ]
 
-function buildJSONStringForSkillOutput(skillArray, skillTable){
+for(let i = 0; i < skillsIdArray.length; i += 1){
+  let tempReadCont = fs.readFileSync(`../dynamoDB_mock_data_returns/${textArray[i][1]}`, 'utf8');
+  skillsIdArray[i] = JSON.parse(tempReadCont)
+}
+
+currentIdsForSkillsLanguages = skillsIdArray[0];
+currentIdsForSkillsProfessional = skillsIdArray[1];
+currentIdsForSkillsSoftware = skillsIdArray[2];
+
+for(let i = 0; i < skillsIdArray.length; i += 1){
+  if(skillsIdArray[i].length === textArray[i][0].length){
+    console.log('Arrays are Equal Length');
+  }
+  else {
+    let tempArr = [];
+    textArray[i][0].forEach(skillItem => tempArr.push(uuidv1()));
+    tempArr = JSON.stringify(tempArr)
+    fs.writeFileSync(`../dynamoDB_mock_data_returns/${textArray[i][1]}`, tempArr, 'utf8')
+  }
+}
+
+function buildJSONStringForSkillOutput(skillIdArray, skillArray, skillTable){
     let string = {
 
     }
     string[skillTable] = []
     for(let i = 0; i < skillArray.length; i += 1){
+        // build an array of Users with Skill
+        let usersWithSkillArr = [];
+        let userIdArray = [...currentIdsForUsers];
+        let loopLength = allFunctions.getRandomNumber(currentIdsForUsers.length)
+        
+        for(let i = 0; i < loopLength; i += 1){
+          let randomIndex = allFunctions.getRandomIndexOfArray(userIdArray.length);
+          let tempString = {
+            "S": userIdArray[randomIndex]
+          }
+          usersWithSkillArr.push(tempString);
+          userIdArray.splice(randomIndex, 1)
+        }
         let tempObj = {
-            PutRequest:{
-                Item: { 
-                    Id: { S: skillArray[i][0]},
-                    value: { M: {
-                        name: { S: skillArray[i][1]},
-                        users: { L: []}
-                        }
-                    }
-                }
+          PutRequest:{
+            Item: { 
+              Id: { S: skillIdArray[i] },
+              name: { S: skillArray[i] },
+              _usersWithSkill: { L: usersWithSkillArr }
             }
+          }
         }
         string[skillTable].push(tempObj)
     }
@@ -65,12 +96,23 @@ function buildJSONStringForSkillOutput(skillArray, skillTable){
     let JSONString = JSON.stringify(string)
     console.log(JSONString);
     fs.writeFileSync(`../../recursive_thinking_server/db_fill/${skillTable}.json`, JSONString, 'utf8')
+    // read back the db fill file
+    let tempReadArr = fs.readFileSync(`../../recursive_thinking_server/db_fill/${skillTable}.json`, 'utf8');
+    let parseTempReadObj = JSON.parse(tempReadArr);
+    // console.log(parseTempReadObj);
+    let tempArr = []
+    for(let i = 0; i < parseTempReadObj[skillTable].length; i += 1){
+      let temp = AWS.DynamoDB.Converter.unmarshall(parseTempReadObj[skillTable][i]['PutRequest']['Item']);
+      tempArr.push(temp)
+    }
+    tempArr = JSON.stringify(tempArr)
+    fs.writeFileSync(`../dynamoDB_mock_data_returns/${skillTable}.json`, tempArr, 'utf8');
+    fs.writeFileSync(`../../recursive_thinking_website_react_sandbox/main/data_returns/${skillTable}.json`, tempArr, 'utf8');
 }
 
-buildJSONStringForSkillOutput(allSkillsLanguageArray, 'RecursiveThinkingProfileSkillsLanguage')
-buildJSONStringForSkillOutput(allSkillsProfessionalArray, 'RecursiveThinkingProfileSkillsProfessional')
-buildJSONStringForSkillOutput(allSkillsSoftwareArray, 'RecursiveThinkingProfileSkillsSoftware')
-
+buildJSONStringForSkillOutput(currentIdsForSkillsLanguages, allSkillsLanguageArray, 'RecursiveThinkingProfileSkillsLanguage')
+buildJSONStringForSkillOutput(currentIdsForSkillsProfessional, allSkillsProfessionalArray, 'RecursiveThinkingProfileSkillsProfessional')
+buildJSONStringForSkillOutput(currentIdsForSkillsSoftware, allSkillsSoftwareArray, 'RecursiveThinkingProfileSkillsSoftware')
 
 // let regexS = /("S")/gi;
 // let regexL = /("L")/gi
